66b815f9bf55087f02c3fd907b0495288bc97d66
BestVersionHimni <oleinikas20@gmail.com>
2024-08-02 23:51:22 +0200

    Function to pull together all photos

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -5,6 +5,7 @@ from PIL import Image
 import numpy as np
 import shutil
 
+
 def get_embeddings(target_dir,source_dir):
 
     if any(os.scandir(target_dir)):
@@ -55,23 +56,40 @@ def get_encodings_selected_face(source_dir):
 
 
 def find_matches(embed_dir, reference_embeddings, tolerance_threshold):
+    if not isinstance(embed_dir, list):
+        matches = []
+        for filename in os.listdir(embed_dir):
+            encoding_path = os.path.join(embed_dir, filename)
+            with open(encoding_path, 'rb') as file:
+                face_encoding = pickle.load(file)
+            results = [face_recognition.compare_faces([face_encoding], ref_encoding, tolerance=tolerance_threshold) for ref_encoding in reference_embeddings]
+            results = [result[0] for result in results]
+            if any(results):
+                original_photo = "_".join(filename.split("_")[:-1])
+                matches.append(original_photo)
+        matches = list(set(matches))
+    else:
+        for face_encoding in embed_dir:
+            results = [face_recognition.compare_faces([face_encoding], ref_encoding, tolerance=tolerance_threshold) for ref_encoding in reference_embeddings]
+            results = [result[0] for result in results]
+            if any(results):
+                original_photo = "_".join(filename.split("_")[:-1])
+                matches.append(original_photo)
+            matches = list(set(matches))
 
-    matches = []
-    for filename in os.listdir(embed_dir):
-        encoding_path = os.path.join(embed_dir, filename)
-        with open(encoding_path, 'rb') as file:
-            face_encoding = pickle.load(file)
-        results = [face_recognition.compare_faces([face_encoding], ref_encoding, tolerance=tolerance_threshold) for ref_encoding in reference_embeddings]
-        results = [result[0] for result in results]
-        if any(results):
-            original_photo = "_".join(filename.split("_")[:-1])
-            matches.append(original_photo)
-    matches = list(set(matches))
-    return matches
+        return matches
 
-
+def pull_together_photos():
+    for album in os.listdir(origin_photos_dir):
+        old_photos_path = []
+        new_photos_path = []
+        old_photos_path = [f"{origin_photos_dir}{album}/{photo_path}" for photo_path in os.listdir(os.path.join(origin_photos_dir,album))]
+        new_photos_path = [f"{all_photos}{photo_path}" for photo_path in os.listdir(os.path.join(origin_photos_dir,album))]
+        for n in range(len(old_photos_path)):
+            shutil.copy(old_photos_path[n],new_photos_path[n])
+            
 #Here we define all the directories that can be used. 
-origin_photos_dir = "TestinngRecog/"#This is the directory with all 6 000 photos 
+origin_photos_dir = "backup/"#This is the directory with all 6 000 photos 
 small_batch_photos_dir = "TestingRecog2/" '''This is directory with the small batch
                                              with only 50 photos.Ensured there are 
                                              my faces in some of those'''
@@ -82,17 +100,8 @@ found_face_photos = "FoundFaceExamples/"
 selected_face_dir = "my_face/"
 testing_faces_dir = "testing_batch/"
 testing_embed_dir = "testing_batch/embeddings_testing/"
+all_photos = "all_photos/"
 
 
 tolerance_threshold = 0.5
-
-
-#Now we only work with the smaller batch, as it is less time consuming
-get_embeddings(target_dir=testing_embed_dir,source_dir=testing_faces_dir)
-
-reference_embeddings = get_encodings_selected_face(source_dir=selected_face_dir)
-
-found_matches = find_matches(embed_dir=testing_embed_dir, reference_embeddings=reference_embeddings, tolerance_threshold=tolerance_threshold)
-
-copy_matched_photos(matched_photos=found_matches,source_photos_dir=testing_faces_dir,target_dir=found_face_photos)
 
